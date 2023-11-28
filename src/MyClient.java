1314121dfdaa865ba2cc524f8114239eb74bd865
Szymon Hudziak <szymonh.dg@gmail.com>
2023-11-28 21:03:30 +0100

    add simple data generation for Sensor class

--- src/MyClient.java
+++ src/MyClient.java
@@ -1,41 +1,41 @@
-import com.prosysopc.ua.DataTypeConversionException;
-import com.prosysopc.ua.ServiceException;
-import com.prosysopc.ua.SessionActivationException;
-import com.prosysopc.ua.StatusException;
-import com.prosysopc.ua.client.ConnectException;
-import com.prosysopc.ua.client.InvalidServerEndpointException;
-import com.prosysopc.ua.client.UaClient;
-import com.prosysopc.ua.stack.builtintypes.DataValue;
-import com.prosysopc.ua.stack.builtintypes.NodeId;
-import com.prosysopc.ua.stack.builtintypes.UnsignedInteger;
-import com.prosysopc.ua.stack.transport.security.SecurityMode;
 
 public class MyClient {
 
-	public static void main(String[] args) throws InvalidServerEndpointException, ConnectException, SessionActivationException, ServiceException, StatusException, InterruptedException, DataTypeConversionException {
-		String address = "opc.tcp://LAPTOP-7499MVRF:53530/OPCUA/SimulationServer";
-		UaClient client = new UaClient();
-		NodeId myNode = new NodeId(3, 1001);
+	public static void main(String[] args) throws InterruptedException{
+		Sensor t_sens = new Sensor(10, 100);
+		Thread t_thread = new Thread(t_sens);
+		t_thread.setDaemon(true);
 		
-		client.setAddress(address);
-		client.setSecurityMode(SecurityMode.NONE);
-		client.connect();
-		System.out.println("Connected");
+		t_thread.start();
+		
 		for(int i = 0; i < 10; i++)
 		{
-			client.writeAttribute(myNode, UnsignedInteger.valueOf(13), i, true);
-			Thread.sleep(1000);
+			System.out.println(t_sens.getValue());
+			Thread.sleep(500);
 		}
-//		DataValue value;
-//		for(int i = 0; i < 5; i++)
+//		String address = "opc.tcp://LAPTOP-7499MVRF:53530/OPCUA/SimulationServer";
+//		UaClient client = new UaClient();
+//		NodeId myNode = new NodeId(3, 1001);
+//		
+//		client.setAddress(address);
+//		client.setSecurityMode(SecurityMode.NONE);
+//		client.connect();
+//		System.out.println("Connected");
+//		for(int i = 0; i < 10; i++)
 //		{
-//			value = client.readValue(myNode);
-//	        System.out.println(value.getValue()); 
-//	        Thread.sleep(1000);
+//			client.writeAttribute(myNode, UnsignedInteger.valueOf(13), i, true);
+//			Thread.sleep(1000);
 //		}
-		
-		client.disconnect();
-		System.out.println("Disconnected");
+////		DataValue value;
+////		for(int i = 0; i < 5; i++)
+////		{
+////			value = client.readValue(myNode);
+////	        System.out.println(value.getValue()); 
+////	        Thread.sleep(1000);
+////		}
+//		
+//		client.disconnect();
+//		System.out.println("Disconnected");
 	}
 
 }
--- src/Sensor.java
+++ src/Sensor.java
@@ -1,23 +1,41 @@
+import java.util.Random;
 
-public class Sensor {
+public class Sensor implements Runnable{
 	private float value;
-	Sensor()
+	private float lowerBound;
+	private float upperBound;
+	
+	Random rand = new Random();
+	
+	Sensor(float lowerBound, float upperBound)
 	{
-		
-	}
-	private void run()
-	{
-		
-	}
+		this.lowerBound = lowerBound;
+		this.upperBound = upperBound;
+	};
 	
 	private void generateValue()
 	{
-		
+		this.value = rand.nextFloat(this.lowerBound, this.upperBound);
 	}
 	
 	public float getValue() 
 	{
 		return this.value;
+	}
+
+	@Override
+	public void run() 
+	{
+		while(true)
+		{
+			this.generateValue();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
