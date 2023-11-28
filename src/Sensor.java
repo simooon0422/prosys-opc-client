501cfb28620599864815e88aa403a632033ed759
Szymon Hudziak <szymonh.dg@gmail.com>
2023-11-28 17:55:39 +0100

    create Sensor class

---
+++ src/Sensor.java
@@ -0,0 +1,23 @@
+
+public class Sensor {
+	private float value;
+	Sensor()
+	{
+		
+	}
+	private void run()
+	{
+		
+	}
+	
+	private void generateValue()
+	{
+		
+	}
+	
+	public float getValue() 
+	{
+		return this.value;
+	}
+
+}
